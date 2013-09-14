70e3430849951250bdcbef82546e91bba5f9b7f7
taiz7715 <taiz7715@gmail.com>
2013-09-14 14:07:25 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enpit::Application.config.secret_token = 'f8a3ba9ebcee02b539d73848e007ee95b1fffde75d3423cb2fd3ce67246a62bde9c2ef631d40d75d48abb138cbdadb577cf88695bc8ed382606d5f347b53b094'
