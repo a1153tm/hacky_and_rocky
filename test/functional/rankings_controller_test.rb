8deddd9e6216c33fbad91d0a112323da8324ae60
a1153tm <a1153tm@aiit.ac.jp>
2013-09-22 06:36:42 +0000

    push test from vagrant

--- test/functional/rankings_controller_test.rb
+++ test/functional/rankings_controller_test.rb
@@ -5,45 +5,9 @@ class RankingsControllerTest < ActionController::TestCase
     @ranking = rankings(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:rankings)
-  end
-
-  test "should get new" do
-    get :new
+  test "should get fetch" do
+    get :fetch
     assert_response :success
   end
 
-  test "should create ranking" do
-    assert_difference('Ranking.count') do
-      post :create, ranking: { xml: @ranking.xml }
-    end
-
-    assert_redirected_to ranking_path(assigns(:ranking))
-  end
-
-  test "should show ranking" do
-    get :show, id: @ranking
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @ranking
-    assert_response :success
-  end
-
-  test "should update ranking" do
-    put :update, id: @ranking, ranking: { xml: @ranking.xml }
-    assert_redirected_to ranking_path(assigns(:ranking))
-  end
-
-  test "should destroy ranking" do
-    assert_difference('Ranking.count', -1) do
-      delete :destroy, id: @ranking
-    end
-
-    assert_redirected_to rankings_path
-  end
 end
