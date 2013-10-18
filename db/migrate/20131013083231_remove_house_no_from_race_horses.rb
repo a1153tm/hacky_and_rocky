c532126d3745cff1189c32db7872b7e6ae0dbbc9
Noguchi Hiroshi <a1316hn@aiit.ac.jp>
2013-10-18 09:49:03 +0000

    travis

--- db/migrate/20131013083231_remove_house_no_from_race_horses.rb
+++
@@ -1,5 +0,0 @@
-class RemoveHouseNoFromRaceHorses < ActiveRecord::Migration
-  def change
-    remove_column :race_horses, :house_no, :integer
-  end
-end
--- db/migrate/20131013083256_add_horse_no_from_race_horses.rb
+++
@@ -1,5 +0,0 @@
-class AddHorseNoFromRaceHorses < ActiveRecord::Migration
-  def change
-    add_column :race_horses, :horse_no, :integer
-  end
-end
