b35314979fe64e40dbe3a68bacdecdd1b27bb0bb
Noguchi Hiroshi <a1316hn@aiit.ac.jp>
2013-10-18 23:39:38 +0000

    login check...

--- app/controllers/voting_cards_controller.rb
+++ app/controllers/voting_cards_controller.rb
@@ -1,6 +1,6 @@
 class VotingCardsController < ApplicationController
   
-  before_action :login_check, only:[:entry]
+  #before_action :login_check, only:[:entry]
   before_action :entry_success_check, only:[:entry]
   before_action :race_undefined_check, only:[:entry]
 
@@ -19,25 +19,29 @@ class VotingCardsController < ApplicationController
   #投票データを格納する。
   #route POST race/:id
   def entry
-    @race = Race.find(params[:id], :include => :race_horses)
-    begin
-      ActiveRecord::Base.transaction do
-        @voting_card = VotingCard.new(:race_id => @race.id , :user_id => current_user.id, :vote_date => Time.now)
-        if @voting_card.save!
-          @voting_card = VotingCard.find(:last)
-          for i in 1..@race.race_horses.size
-            voteItem = VoteItem.new(:voting_card_id => @voting_card.id,:race_horse_id => params['horse_id'+ i.to_s ],:point_weight => params['point_weight'+ i.to_s ])
-            voteItem.save!
+    if current_user
+      @race = Race.find(params[:id], :include => :race_horses)
+      begin
+        ActiveRecord::Base.transaction do
+          @voting_card = VotingCard.new(:race_id => @race.id , :user_id => current_user.id, :vote_date => Time.now)
+          if @voting_card.save!
+            @voting_card = VotingCard.find(:last)
+            for i in 1..@race.race_horses.size
+              voteItem = VoteItem.new(:voting_card_id => @voting_card.id,:race_horse_id => params['horse_id'+ i.to_s ],:point_weight => params['point_weight'+ i.to_s ])
+              voteItem.save!
+            end
           end
         end
+        #vote_item登録成功時の処理
+        @error = 'あなたの投票状況です。'
+        render 'races/show'
+      rescue
+        #登録失敗時
+        @error = '登録できませんでした'
+        render 'races/show'
       end
-      #vote_item登録成功時の処理
-      @error = 'あなたの投票状況です。'
-      render 'races/show'
-    rescue
-      #登録失敗時
-      @error = '登録できませんでした'
-      render 'races/show'
+    else
+      redirect_to :controller => 'races' , :action => 'index'
     end
   end
   
