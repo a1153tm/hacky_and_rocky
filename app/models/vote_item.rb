be24a7a4d8d4a4ccb6ad1ef94cee39f4aa42718f
a1153tm <a1153tm@aiit.ac.jp>
2013-10-29 11:29:26 +0000

    closed #51 closed #59 closed #60

--- app/models/vote_item.rb
+++ app/models/vote_item.rb
@@ -1,6 +1,15 @@
 class VoteItem < ActiveRecord::Base
-  EXPECTATION = {1 => '◎　本命' , 2 => '○　対抗' , 3 =>'△　穴ヒモ'}
+
+  EXPECTATION = {10 => '◎　本命' , 5 => '○　対抗' , 3 =>'△　穴ヒモ'}
+
   belongs_to :voting_card
   belongs_to :race_horse
-  validates :race_horse_id , :point_weight , presence: true #必須
+
+  validates :race_horse_id , :point_weight , presence: true
+  validates_each :point_weight do |record, attr, weight|
+    unless EXPECTATION.keys.include? weight.to_i
+      record.errors[attr] << "任意の値を投票することはできません。"
+    end
+  end
+
 end
--- test/models/voting_card_test.rb
+++ test/models/voting_card_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class VotingCardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "voting_card attributes must not be empty" do
+    card = VotingCard.new
+    assert card.invalid?
+    assert card.errors[:race_id].any?
+    assert card.errors[:vote_date].any?
+    assert card.errors[:user_id].any?
+    assert card.errors[:vote_items].any?
+  end
+
+  test "voting_card must have one vote_item at least" do
+    card = VotingCard.new(race_id: 1, vote_date: Time.now, user_id: 1, vote_items: [])
+    assert card.invalid?
+    assert card.errors[:vote_items].any?
+  end
+
+  test "voting_card must have honmei" do
+    honmei = VoteItem::EXPECTATION.first[0]
+    item = VoteItem.new(race_horse_id: 1, point_weight: VoteItem::EXPECTATION.keys[1])
+    card = VotingCard.new(race_id: 1, vote_date: Time.now, user_id: 1)
+    card.vote_items << item
+    assert card.invalid?
+    assert card.errors[:vote_items].any?
+    item = VoteItem.new(race_horse_id: 1, point_weight: VoteItem::EXPECTATION.keys[0])
+    card.vote_items << item
+    assert card.valid?
+  end
+
+  test "vote_items in voting_card must be uniq" do
+    honmei = VoteItem::EXPECTATION.first[0]
+    card = VotingCard.new(race_id: 1, vote_date: Time.now, user_id: 1)
+    card.vote_items << VoteItem.new(race_horse_id: 1, point_weight: VoteItem::EXPECTATION.keys[0])
+    card.vote_items << VoteItem.new(race_horse_id: 2, point_weight: VoteItem::EXPECTATION.keys[0])
+    assert card.invalid?
+    assert card.errors[:vote_items].any?
+  end
+
 end
