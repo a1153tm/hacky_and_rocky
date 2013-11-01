2d98370e1afa27f65d728828f8c1ffead0fa413b
a1153tm <a1153tm@aiit.ac.jp>
2013-11-01 17:29:57 +0000

    #53 closed ,#57 closed

--- app/controllers/race_progresses_controller.rb
+++ app/controllers/race_progresses_controller.rb
@@ -2,13 +2,20 @@ class RaceProgressesController < ApplicationController
   
   def show
     flash[:error] = "レースが存在しません。" unless @race = Race.find_by_id(params[:id])
-    @prog = @race.progress(:last)
     
-    # @Todo ここは予定に合わせて変更する。一応↓のコードは変な日付や文字が入っていたら、一番最後のレコードを返すようにしている。
+    # @TODO　ラスト前提で現在は構成しているため。現在は日付までは追っていない 
+    flash[:error] = "レース状況がありません" unless RaceProgress.find_by(:race_id => params[:id])
+
+    # @TODO ここは予定に合わせて変更する。一応↓のコードは変な日付や文字が入っていたら、一番最後のレコードを返すようにしている。  
     #if /\d{4}-\d{2}-\d{2}/ =~ params[:date]
     #then @prog = @race.progress(DateTime.parse(params[:date]).to_date) 
     #else @prog = @race.progress(:last)
     #end
+    
+    unless flash[:error]
+      @prog = @race.progress(:last)
+    end
+
   end
   
 end
--- app/helpers/race_progresses_helper.rb
+++ app/helpers/race_progresses_helper.rb
@@ -1,13 +1,2 @@
 module RaceProgressesHelper
-  
-  #出走の色付け -> show
-  def horse_number_color(number)
-    horseNumberColors = ['white','black','red','blue','orange','green','pink','skyblue','yellow','limegreen']
-    currentColor = nil
-    if horseNumberColors.size - 1 >= number
-      currentColor = horseNumberColors[number]
-    end
-    return currentColor
-  end
-  
 end
