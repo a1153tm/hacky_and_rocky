5ef9ba04a0f42901e2703cc2d79fd6f6f44d05ad
a1153tm <a1153tm@aiit.ac.jp>
2013-11-01 11:02:36 +0000

    travis

--- app/controllers/race_progresses_controller.rb
+++ app/controllers/race_progresses_controller.rb
@@ -1,6 +1,11 @@
 class RaceProgressesController < ApplicationController
+  
   def show
-    flash[:error] = "レースが存在しません。" unless @race = Race.find(params[:id])
-    @prog = @race.progress(:last)
+    flash[:error] = "レースが存在しません。" unless @race = Race.find_by_id(params[:id])
+    if /\d{4}-\d{2}-\d{2}/ =~ params[:date]
+    then @prog = @race.progress(DateTime.parse(params[:date]).to_date) 
+    else @prog = @race.progress(:last)
+    end
   end
+  
 end
