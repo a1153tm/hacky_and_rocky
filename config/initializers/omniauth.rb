788d4f523bb15b7d238d3d8b665ebcb8dabf7b80
a1153tm <a1153tm@aiit.ac.jp>
2013-11-02 06:23:46 +0000

    for denka-test envorinment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.production?
+  if Rails.env.production? and !ENV['TEST']
   then provider :facebook, "651646748202839", "3bcefe12c7ba6d497c834178b25da5d9"
   else provider :developer
   end
