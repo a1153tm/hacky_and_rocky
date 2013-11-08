da533a65e205bf51cdd91f61a75144797db99391
Masahiko Takahashi <a1134mt@aiit.ac.jp>
2013-11-08 09:58:31 +0000

    race title modified test

--- app/models/vote_item.rb
+++ app/models/vote_item.rb
@@ -1,6 +1,6 @@
 class VoteItem < ActiveRecord::Base
 
-  EXPECTATION = {10 => '本命' , 5 => '対抗' , 3 =>'ヒモ'}
+  EXPECTATION = {10 => '本命' , 5 => '対抗' , 3 =>'アナ'}
 
   belongs_to :voting_card
   belongs_to :race_horse
