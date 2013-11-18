841013e8191a866b65affc93073bcf0255aa08db
Masahiko Takahashi <a1134mt@aiit.ac.jp>
2013-11-18 12:47:04 +0000

    pick_book_script modified

--- lib/tasks/pick_book_task.rb
+++ lib/tasks/pick_book_task.rb
@@ -2,7 +2,7 @@ require './app/models/book'
 require './app/models/genre'
 
 class PickBookTask
-  def self.execute(item_code = "11301985")
+  def self.execute(item_code = "11807411")
     #Genre.find(:all).each do |g|
       begin
         item_json = JSON.parse(get_item_json(item_code))
