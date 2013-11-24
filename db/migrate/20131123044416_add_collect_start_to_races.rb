73ce25f0ba6be2b3ac6d1ed82e6e9a9627a5baa2
a1153tm <a1153tm@aiit.ac.jp>
2013-11-24 05:57:46 +0000

    change migration default values

--- db/migrate/20131123044416_add_collect_start_to_races.rb
+++ db/migrate/20131123044416_add_collect_start_to_races.rb
@@ -1,5 +1,6 @@
 class AddCollectStartToRaces < ActiveRecord::Migration
   def change
     add_column :races, :collect_start, :timestamp
+    Race.update_all ["collect_start = ?", Time.now]
   end
 end
