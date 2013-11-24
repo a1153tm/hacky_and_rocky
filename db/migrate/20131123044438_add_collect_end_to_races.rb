f4afe9a7101face9935876129e2985daa58448dd
a1153tm <a1153tm@aiit.ac.jp>
2013-11-24 06:18:09 +0000

    change migration default values

--- db/migrate/20131123044438_add_collect_end_to_races.rb
+++ db/migrate/20131123044438_add_collect_end_to_races.rb
@@ -1,6 +1,6 @@
 class AddCollectEndToRaces < ActiveRecord::Migration
   def change
     add_column :races, :collect_end, :timestamp
-    Race.update_all ["collect_end = ?", "end_date"]
+    Race.update_all ["collect_end = end_date"]
   end
 end
