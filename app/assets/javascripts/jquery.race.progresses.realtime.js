382e9b0c46d625fab8cbee6e355a75ee69d6f078
a1153tm <a1153tm@aiit.ac.jp>
2013-12-13 16:48:55 +0000

    fixed race canvas bug

--- app/assets/javascripts/jquery.race.progresses.realtime.js
+++ app/assets/javascripts/jquery.race.progresses.realtime.js
@@ -21,6 +21,7 @@ var RaceCanvas = Backbone.View.extend({
     
     drawTrac: function() {
         this.ctx.beginPath();
+        this.ctx.strokeStyle = 'black';
         this.ctx.arc(this.HANKEI + this.YOHAKU, this.HANKEI + this.YOHAKU_UE, this.HANKEI, 0.5 * Math.PI, 1.5 * Math.PI, false);
         this.ctx.lineTo(this.YOHAKU + this.HANKEI + this.STRAIT, this.YOHAKU_UE);
         this.ctx.arc(this.YOHAKU + this.HANKEI + this.STRAIT, this.HANKEI + this.YOHAKU_UE, this.HANKEI, -0.5 * Math.PI, 0.5 * Math.PI, false);
@@ -88,16 +89,17 @@ var RaceCanvas = Backbone.View.extend({
             img.src = horse.book.small_image_url;
             return img.onload = function() {
                 var rectHeight = 18;
-                //_this.ctx.beginPath();
-                //_this.ctx.rect(x, y - rectHeight, img.width, rectHeight);
-                //_this.ctx.fillStyle = horse.color.background_color;
-                //_this.ctx.fill();
-                //_this.ctx.font = "90% Sans-Serif";
-                //_this.ctx.strokeStyle = horse.color.text_color;
-                //_this.ctx.textAlign = 'center';
-                //_this.ctx.strokeText(horse.book.title.substr(0,3), x + img.width / 2, y - rectHeight + 12);
-                _//this.ctx.strokeText("1", x + img.width / 2, y - rectHeight + 12);
-                return _this.ctx.drawImage(img, x, y);
+                _this.ctx.drawImage(img, x, y);
+                _this.ctx.beginPath();
+                _this.ctx.rect(x, y - rectHeight, img.width, rectHeight);
+                _this.ctx.fillStyle = horse.color.background_color;
+                _this.ctx.fill();
+                _this.ctx.closePath();
+                _this.ctx.font = "90% Sans-Serif";
+                _this.ctx.strokeStyle = horse.color.text_color;
+                _this.ctx.textAlign = 'center';
+                _this.ctx.strokeText(horse.book.title.substr(0,3), x + img.width / 2, y - rectHeight + 12);
+                _this.ctx.stroke();
             };
         });
     }
